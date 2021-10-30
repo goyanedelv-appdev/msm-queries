1ed9025f5c3fe0d2419b167a3a606ada07a9aae9
Gonzalo Oyanedel Vial <goyanedelv@gmail.com>
2021-10-30 18:32:36 +0000

    full video

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   get("/directors/eldest", { :controller => "directors", :action => "wisest" })
 
+  get("/directors/:an_id", { :controller => "directors", :action => "director_details" })
+
+  # Static route that could be mapped by a dynamic route must be first in the code (top down peiority)
+
   get("/", { :controller => "application", :action => "homepage" })
 
   get("/directors", {:controller => "directors", :action => "index" })
