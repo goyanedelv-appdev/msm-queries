f78c4de304f7c1e3576f518447aa8897991ca766
Gonzalo Oyanedel Vial <goyanedelv@gmail.com>
2021-10-30 00:48:21 +0000

    until min 1:09:00 - index.html.erb not working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  get("/directors/eldest", { :controller => "directors", :action => "wisest" })
+
   get("/", { :controller => "application", :action => "homepage" })
+
+  get("/directors", {:controller => "directors", :action => "index" })
+  #get("/actors", {:controller => "actors", :action => "index" })
+  #get("/movies", {:controller => "movies", :action => "index" })
+  #get("/characters", {:controller => "characters", :action => "index" })
+
 end
